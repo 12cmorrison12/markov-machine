d1769e8d83e74f6554e64c473d7f6bbeaa4917c3
Carver Morrison <12cmorrison12@gmail.com>
2023-07-20 18:22:12 -0600

    finished makeText file

--- makeText.js
+++ makeText.js
@@ -1,11 +1,58 @@
 /** Command-line tool to generate Markov text. */
 
-import { require } from 'fs';
+import { require, readFile } from 'fs';
 import { process } from 'axios';
 
 const markov = require("./markov");
-const axios = require("axios");
 
 
+/**generate text from markov machine */
 
+function generateText(text) {
+    let mm = markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
 
+/**read text file to make text from it */
+
+function makeText(path) {
+    readFile(path, "utf8", function cb(error, data) {
+        if (error) {
+            console.error(`Cannot read file: ${path}: ${error}`);
+            exit(1);
+        } else {
+            generateText(data);
+        }
+    });
+}
+
+/** read url provided */
+
+async function makeURLText(url) {
+    let response;
+
+    try {
+        response = await get(url);
+    } catch (error) {
+        console.error(`Cannot read URL: ${url}: ${error}`);
+        exit(1);
+    }
+    generateText(response.data)
+}
+
+/** interpret cmdline to decide what to do. */
+
+let [method, path] = process.argv.slice(2);
+
+if (method === "file") {
+  makeText(path);
+}
+
+else if (method === "url") {
+  makeURLText(path);
+}
+
+else {
+  console.error(`Unknown method: ${method}`);
+  exit(1);
+}
